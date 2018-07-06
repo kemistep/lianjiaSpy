422047d5aaae7ecb255ddba7616c9d8d342596ff
orangeCzx <15022113870@163.com>
2018-07-06 17:09:54 +0800

    实现翻页功能

--- lianjiaSpider/lianjiaSpider/spiders/lianjia.py
+++ lianjiaSpider/lianjiaSpider/spiders/lianjia.py
@@ -11,36 +11,42 @@ from lianjiaSpider.items import LianjiaspiderItem
 class LianjiaSpider(CrawlSpider):
     name = 'lianjia'
     allowed_domains = ['lianjia.com']
-    start_urls = ['https://bj.lianjia.com/zufang/pg1l1/']
+    start_urls = ['https://bj.lianjia.com/zufang/pg1/']
 
     rules = (
-        # Rule(LinkExtractor( restrict_xpaths='//div[@class="option-list"]//a'), callback='next_page'),
         Rule(LinkExtractor(
-                           restrict_xpaths='//ul[@id="house-lst"]/li/div[2]/h2/a'), callback='parse_item'),
+            # allow='/\w+/\w+/',
+            restrict_xpaths='//div[@class="bd"]/dl[1]/dd/div/a[1]'),
+            callback='next_page'),
+        # Rule(LinkExtractor(restrict_xpaths='//div[@class="bd"]/dl[1]/dd/div/a[1]'), callback='next_page'),
+        # Rule(LinkExtractor(restrict_xpaths='//ul[@id="house-lst"]/li/div[2]/h2/a'), callback='parse_item',
+        #      follow=False),
     )
+
     # allow = ('https://bj.lianjia.com/zufang/\d+.html'),
     def next_page(self, response):
         page_url = response.xpath('//@page-url').extract_first()
         page_data = response.xpath("//@page-data").extract_first()
-        total_page = eval(page_data)['totalPage']
-        for page in range(1, total_page):
-            rel_url = page_url.format(page=page)
-            yield Request(url=response.urljoin(rel_url), callback=self.parse_item)
+        if page_url == None:
+            pass
+        else:
+            total_page = eval(page_data)['totalPage']
+            for page in range(1, total_page):
+                rel_url = page_url.format(page=page)
+                yield Request(url=response.urljoin(rel_url), callback=self.parse_item)
+
         '''
         page_data : /zufang/pg{page}l1/
         total_page : 86
         rel_url : /zufang/pg1l1/
         url : 'https://bj.lianjia.com/zufang/pg1l1/'
         '''
-        # for page in range(1, 86):
-        #     rel_url = 'https://bj.lianjia.com/zufang/pg' + str(page) + 'l1'
-        #     yield Request(url=rel_url, callback=self.parse_item)
 
     def parse_item(self, response):
         # for list in response.xpath("//ul[@id='house-lst']/li"):
         l = ItemLoader(item=LianjiaspiderItem(), response=response, )
         l.default_output_processor = Join()
-        # l.add_xpath('url', '//ul[@id="house-lst"]/li/div[2]/h2/a/@href')
-        l.add_xpath('url', '//div[@class="content zf-content"]/div[1]/span[1]/text()')
+        l.add_xpath('url', '//ul[@id="house-lst"]/li/div[2]/div[2]/div[1]/span/text()')
+        # l.add_xpath('url', '//div[@class="content zf-content"]/div[1]/span[1]/text()')
         item = l.load_item()
         yield item
