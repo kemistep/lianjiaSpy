5452288406a3681e8d9fd7ed70c495d07ce147dc
orangeCzx <40862634+orangeCzx@users.noreply.github.com>
2018-07-10 12:18:17 +0800

    Update lianjia.py

--- lianjiaSpider/lianjiaSpider/spiders/lianjia.py
+++ lianjiaSpider/lianjiaSpider/spiders/lianjia.py
@@ -31,9 +31,9 @@ class LianjiaSpider(CrawlSpider):
                 yield Request(url=response.urljoin(rel_url), callback=self.parse_item)
 
         '''
-        page_data : /zufang/pg{page}l1/
+        page_data : /zufang/pg{page}/
         total_page : 86
-        rel_url : /zufang/pg1l1/
+        rel_url : /zufang/pg1/
         url : 'https://bj.lianjia.com/zufang/pg1/'
         '''
 
